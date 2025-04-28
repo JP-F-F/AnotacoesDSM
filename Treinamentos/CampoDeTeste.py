87f369dbd586adcce630e3057a7015c481510bd7
juan <juan.ferreira3@fatec.sp.gov.br>
2025-04-28 20:22:50 -0300

    estudos de algoritmos

--- Treinamentos/CampoDeTeste.py
+++ Treinamentos/CampoDeTeste.py
@@ -1,3 +1,30 @@
-f = float(input())
-valor = f /0.100 * 28
-print(valor)
+#X = int(input('x'))
+#Y = int(input('y'))
+#soma = 0
+
+#if Y < 0:
+#    Y = -Y
+#elif X < 0:
+#    X = -X
+#elif Y < 0 and X < 0:
+#    X = X
+#    Y = Y
+
+#if Y > X:
+#    while X < Y:
+#        if X % 2 != 0:
+#            soma += X
+#            X += 2
+#        elif X % 2 == 0:
+#            X += 1
+        
+#elif X > Y:
+#    while Y < X:
+#        if Y % 2 != 0:
+#            soma += Y
+#           Y += 2
+#        elif Y % 2 == 0:
+#            Y += 1
+        
+
+#print(soma)###
